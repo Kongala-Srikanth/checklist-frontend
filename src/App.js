2bae66960dcbef5c08cbe53aabc4972898fb1e30
Srikanth <kongala.srikanth44@gmail.com>
2024-11-29 10:20:07 +0530

    Checklist Frontend Developement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Switch, Route, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+
+import ApplyApplication from './components/ApplyApplication';
+import ApplicationLists from './components/ApplicationLists';
+import ApplicationDetails from './components/ApplicationDetails'
+import Home from './components/Home';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/application' component={ApplyApplication} />
+      <Route exact path='/applications/list' component={ApplicationLists} />
+      <Route exact path='/application/:id' component={ApplicationDetails} />
+      <Redirect to='/' />
+    </Switch>
   );
 }
 
